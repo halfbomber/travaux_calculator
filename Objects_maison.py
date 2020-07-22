31b6ed29f1006a7a2536d1cc878dddee2fdb0591
Bagouet Alexandre <bagouet.alex@gmail.com>
2020-07-22 14:45:02 +0200

    first commit

---
+++ Objects_maison.py
@@ -0,0 +1,10 @@
+
+def __init__(self):
+    pass
+
+class ObjMur:
+    def __init__(self):
+        self.Longueur = float()
+        self.Hauteur = float()
+    def surface(self):
+        return (self.Longueur * self.Hauteur)
---
+++ test_mur.py
@@ -0,0 +1,8 @@
+import Objects_maison
+
+
+mur1 = ObjMur()
+mur1.Longueur = 4,6
+mur1.Hauteur = 3,4
+
+print(mur1.surface())
