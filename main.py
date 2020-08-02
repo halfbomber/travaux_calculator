8b12a0f250c88acb612d759623f9782fc39240f2
Bagouet Alexandre <bagouet.alex@gmail.com>
2020-08-02 10:13:27 +0200

    add append fonction

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 #main
 import yaml_loader, os
+"""
+{
+maison: {
+Hauteur: 0,
+Largeur: 0,
+Longueur: 0,
+pieces:
+    [{Salon:
+        {Hauteur: 0,
+        Largeur: 0,
+        Longueur: 0,
+        couleurs: [blanc],
+        mur1: {Largeur: 0, Longueur: 0, couleur: null,type: null},
+        mur2: {Largeur: 0, Longueur: 0, couleur: null, type: null},
+        mur3: {Largeur: 0, Longueur: 0, couleur: null, type: null},
+        mur4: {Largeur: 0,Longueur: 0, couleur: null, type: null},
+        plafon: {Largeur: 0, Longueur: 0,couleur: null, type: null},
+        sol: {Largeur: 0, Longueur: 0, couleur: null,type: null},
+        superficie: null}
+    }],
+terrain: {Largeur: 0, Longueur: 0}}
+}
+"""
 
 if __name__ == '__main__':
     YamlProjetPath = os.getcwd()+'/structure_maison/'
-    # PROJECT_PATH+'/structure_maison/test_architecture.yaml
+    # KeyToModify = ['maison']['Hauteur']
+    # pieces':[{'salon':{'superficie':50}}]}}
+
     YML = yaml_loader.yamlloader(YamlProjetPath)
-    data = YML.open('test_architecture.yaml', Mode='r')
-    YML.show()
-    data.close()
-    # print(data)
-    KeyToModify = 'superficie'
-    Value = 50
-    # YML.append(superficie=Value)
+    MyYaml = YML.load(Stream=YML.open('test_architecture.yaml', Mode='r'))
+    YML.append(Stream=MyYaml,KeysValue={"maison1","Hauteur"=50})
--- yaml_loader.py
+++ yaml_loader.py
@@ -1,26 +1,39 @@
-import yaml, os
+import os, yaml
+# ruamel.yaml
 from yamlinclude import YamlIncludeConstructor as ymlconstructor
 
 class yamlloader():
     def __init__(self, DirPath):
+        # yaml = ruamel.yaml.YAML()
         self.dir_path = DirPath
+        self.datafile = dict()
         ymlconstructor.add_to_loader_class(loader_class=yaml.FullLoader, base_dir=str(self.dir_path))
 
     def open(self, File, Mode='r'):
-        with open(os.path.join(self.dir_path+'/'+File), Mode) as stream:
-            self.datafile = yaml.load(stream, Loader=yaml.FullLoader)
+        # self.rawfile = open(os.path.join(self.dir_path+'/'+File), Mode)
+        return open(os.path.join(self.dir_path+'/'+File), Mode, encoding='utf8')
+        # return self.rawfile
+
+    def load(self, Stream):
+        self.datafile = yaml.load(Stream, Loader=yaml.FullLoader)
         return self.datafile
+        # print(yaml.dump(self.datafile, default_flow_style=FlowStyle))
+    def human(self, FlowStyle=False):
+        return yaml.dump(self.datafile, default_flow_style=FlowStyle)
 
-    def show(self, FlowStyle=False):
-        print(yaml.dump(self.datafile, default_flow_style=FlowStyle))
-
-    def append(self, **KeysValues):
+    def append(self, Stream, FlowStyle=False, AllowUnicode=True, **KeysValues):
         print(KeysValues)
-        for key,value in KeysValues.items() :
-            for datakey in self.datafile :
-                if self.datafile[datakey] == key :
-                    yaml.dump(self.datafile[datakey], value)
-                else:
-                    next
+        for data in self.datafile:
+            print(data)
+            # if data[]
+            # yaml.dump(KeysValues, Stream, default_flow_style = FlowStyle, allow_unicode = AllowUnicode)
+        # # print(KeysValues)
+        # for key,value in KeysValues.items() :
+        #     for datakey in self.datafile :
+        #         if self.datafile[datakey] == key :
+        #             yaml.dump(self.datafile[datakey], value)
+        #         else:
+        #             next
+
     def close(self):
-        close(os.path.join(self.dir_path+'/'+File))
+        self.rawfile.close()
